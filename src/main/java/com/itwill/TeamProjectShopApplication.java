f8456f5b63f1706e959de7399eec86f7c72ace6f
김선혁 <SeonHyuckKim>
2025-10-02 11:04:16 +0900

    ..

---
+++ src/main/java/com/itwill/TeamProjectShopApplication.java
@@ -0,0 +1,13 @@
+package com.itwill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProjectShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProjectShopApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/itwill/shop/dao/MemberDao.java
@@ -0,0 +1,5 @@
+package com.itwill.shop.dao;
+
+public class MemberDao {
+// dao = repository 라고도 함
+}
